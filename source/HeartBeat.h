4695d7e5af8c46199bb9207e0c1ce61b3c9a538f
okeltw <okeltw@mail.uc.edu>
2017-01-14 19:40:20 -0500

    Frameworking: add header files

---
+++ source/HeartBeat.h
@@ -0,0 +1,6 @@
+#ifndef __HEARTBEAT_H
+#define __HEARTBEAT_H
+
+static const int HB_ADDRESS = 0x00; //TODO: find appropriate address
+
+#endif // ifndef __HEARTBEAT_H
---
+++ source/Motion.h
@@ -0,0 +1,6 @@
+#ifndef __MOTION_H
+#define __MOTION_H
+
+static const int M_ADDRESS = 0x00; // TODO: find appropriate address
+
+#endif //ifndef __MOTION_H
