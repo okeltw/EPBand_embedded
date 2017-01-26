b613e925010d078af1454acce7e7cc60660c9473
okeltw <okeltw@mail.uc.edu>
2017-01-25 21:10:43 -0500

    Stuff

--- source/Motion.py
+++ source/Motion.py
@@ -23,3 +23,15 @@ class MotionController(object):
         elif(debug)
             print("Connected to " + ADDRESS)
         # TODO
+
+    def dist(a,b):
+        """
+        Calculate distance of two points
+
+        Doctests:
+        >>> dist(0,0)
+        0
+        >>> dist(666,13.37) #From Tommy, bc he is the devil.
+        666.1341883584718
+        """
+        return math.sqrt((a*a)+(b*b))
