f0b4d96ac1262a71e5ebb023edc5f0f2c4cf89c0
okeltw <okeltw@mail.uc.edu|>
2017-02-06 23:58:48 +0000

    Cleaning up Master branch

--- source/EPBand.py
+++ source/EPBand.py
@@ -9,40 +9,15 @@ from Motion import MotionController
 from Bluetooth import BluetoothController
 import sys
 
-def main(argv):
-    print('starting')
-    debug = False # send this where necessary to enable/disable debug
-    # e.g. if debug: print(<debug info>)
+print('starting')
+try:
+    PC = PulseController()
+    MC = MotionController()
+    BC = BluetoothController()
+except ConnectionError as error:
+    print(error)
+except Exception as error:
+    print("An unknown error occured: ")
+    print(error)
+    quit()
 
-    '''
-    try:
-        opts, args = getopt.getopt(argv)
-    except getopt.GetoptError:
-        print("Incorrect input options")
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt in ("-d", "--debug"):
-            debug = True
-            print("Debug mode enabled.")
-    '''
-    debug = True
-
-    try:
-        PC = PulseController()
-        MC = MotionController(debug)
-        BC = BluetoothController()
-    except ConnectionError as error:
-        print(error)
-    except Exception as error:
-        print("An unknown error occured: ")
-        print(error)
-        quit()
-
-    print('a')
-    print("acceleration X :", MC.X, " scaled: ", MC.X_scl)
-    print("acceleration Y :", MC.Y, " scaled: ", MC.Y_scl)
-    print("acceleration Z :", MC.Z, " scaled: ", MC.Z_scl)
-
-    print("Rotation X: ", MC.get_x_rotation(MC.X_scl, MC.Y_scl, MC.Z_scl))
-    print("Rotation Y: ", MC.get_y_rotation(MC.X_scl, MC.Y_scl, MC.Z_scl))
--- source/Motion.py
+++ source/Motion.py
@@ -8,55 +8,17 @@ class MotionController(object):
     """docstring for ."""
 
     ADDRESS = 0x68 # TODO: actual address
-
-    X_addr = 0x43
-    Y_addr = 0x45
-    Z_addr = 0x47
-
-    X = 0
-    X_scl = 0
-    Y = 0
-    Y_scl = 0
-    Z = 0
-    Z_scl = 0
-
-    scl_factor = 16384.0
-
-    debug = False
-
-    def __init__(self, dbug):
+    def __init__(self):
         print("Initializing Motion...")
         self.connect()
         print("Connected")
-        debug = dbug
 
     def connect(self):
         print("Attempting connection...")
 
         if(False):
             raise ConnectionError("Failed to connect to Motion Sensor")
-        elif(debug):
+        elif(False):
             print("Connected to " + ADDRESS)
         # TODO
 
-    def dist(a,b):
-        return math.sqrt((a*a)+(b*b))
-
-    def get_y_rotation(x,y,z):
-        radians = math.atan2(x, dist(y,z))
-        return -math.degrees(radians)
-
-    def get_x_rotation(x,y,z):
-        radians = math.atan2(y, dist(x,z))
-        return math.degrees
-
-    def read():
-        X = read_word_2c(X_addr)
-        X_scl = X / scl_factor
-        Y = read_word_2c(Y_addr)
-        Y_scl = Y / scl_factor
-        Z = read_word_2c(Z_addr)
-        Z_scl = Z / scl_factor
-        return None
-
-    
