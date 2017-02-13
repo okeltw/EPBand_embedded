e5cc1d9c083d9e44d584bad11624350feaaa5579
AndrewAlbert <andrew.christian.albert@gmail.com>
2017-02-12 20:44:59 -0500

    Updating bluetooth and pulse python files

--- source/Pulse.py
+++ source/Pulse.py
@@ -1,20 +1,70 @@
 """
-The pulse/heartbeat sensor controller.
+The pulse/hearbeat sensor controller.
 """
 
+import RPi.GPIO as GPIO
+import sys
+import time
+from time import sleep
+
 class PulseController(object):
-    """docstring for ."""
-
     ADDRESS = 0x00 # TODO: actual address
-
+    VALUE = GPIO.LOW
+    OLD_VALUE = GPIO.LOW
+    CHANNEL = None
+    
     def __init__(self):
-        print("Initializing Pulse...")
-        self.connect()
-        print("Connected")
+        print("Initializing pulse sensor")
+        #self.setup()
+        #print("Setup")
+        
+    def setup(self, channel):
+        self.CHANNEL = channel
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(channel, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
-    def connect(self):
-        print("Attempting connection...")
+    def measure(self):
+        GPIO.input( self.CHANNEL)
+        if self.VALUE == GPIO.LOW:
+            print("Channel %d is LOW\n" % self.CHANNEL)
+            return 0
+        else:
+            print("Channel %d is HIGH\n" % self.CHANNEL)
+            return 1
 
-        if(False):
-            raise ConnectionError("Failed to connect to Pulse Sensor")
-        # TODO
+    def edge_detect(self):
+        GPIO.wait_for_edge(self.CHANNEL, GPIO.RISING)
+        print("Rising edge\n")
+
+    def monitor(self):
+        while True:
+            self.edge_detect(self)
+            Time = time.strftime("%H:%M:%S",time.gmtime())
+            print("%s\n" % Time)
+
+    def monitor2(self):
+        while True:
+            self.VALUE = GPIO.input( self.CHANNEL)
+            if self.VALUE != self.OLD_VALUE:
+                Time = time.strftime("%H:%M:%S",time.gmtime())
+                self.OLD_VALUE = self.VALUE
+                if self.VALUE == GPIO.LOW:
+                    print("%s LOW" % Time)
+                else:
+                    print("%s HIGH\n" % Time)
+            if self.VALUE == GPIO.HIGH:
+                sleep(0.1)
+
+    def visualize(self):
+        while True:
+            self.VALUE = GPIO.input(self.CHANNEL)
+            if self.VALUE == GPIO.LOW:
+                print("_____\t%s" % time.strftime("%H:%M:%S",time.gmtime()))
+            else:
+                print("|||||\t%s" % time.strftime("%H:%M:%S",time.gmtime()))
+        
+    def cleanup(self):
+        GPIO.cleanup(self.CHANNEL)
+
+
+
